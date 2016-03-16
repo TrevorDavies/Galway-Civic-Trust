2716c2e9f89e9baa494744c45117dc8d86070032
TrevorDavies <trevor.daviesgti@gmail.com>
2016-03-16 18:29:11 +0000

    Moved current app version to TestApps

---
+++ TestApps/GCTWT/www/js/controllers.js
@@ -0,0 +1,15 @@
+
+var app = angular.module('GCTWT.controllers',['ionic'])
+
+.controller('HomeCtrl', function($scope,$state){
+
+})//end HomeCtrl
+.controller('ToursCtrl', function($scope,$state){
+
+})//end HomeCtrl
+.controller('MapsCtrl', function($scope,$state){
+
+})//end HomeCtrl
+.controller('SettingsCtrl', function($scope,$state){
+
+})//end HomeCtrl
