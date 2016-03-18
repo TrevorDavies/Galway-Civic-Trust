d4429808859e8fff0e409717ecfe46ca0eea6932
TrevorDavies <trevor.daviesgti@gmail.com>
2016-03-18 06:03:20 +0000

    update test app

--- TestApps/GCTWT/www/js/controllers.js
+++ TestApps/GCTWT/www/js/controllers.js
@@ -1,15 +1,22 @@
+angular.module('GCTWT.controllers', ['ionic'])
 
-var app = angular.module('GCTWT.controllers',['ionic'])
+.constant('WEATHERAPI_KEY','f40acdcb5aabf0faf07699d42ffc88c2' )
+.controller('HomeCtrl', function($scope,$state,$http,WEATHERAPI_KEY) {
 
-.controller('HomeCtrl', function($scope,$state){
+  $scope.weatherData=[];
+
+  $http.get('http://api.openweathermap.org/data/2.5/weather?id=2964179&units=metric&appid='+WEATHERAPI_KEY).then(function(response){
+    weatherData=response;
+    $scope.temp = Math.round(weatherData.data.main.temp);
+    $scope.current = weatherData.data.weather[0].description;
+
+    console.log(weatherData);
+  }, function(error){
+
+  });
 
 })//end HomeCtrl
-.controller('ToursCtrl', function($scope,$state){
 
-})//end HomeCtrl
-.controller('MapsCtrl', function($scope,$state){
-
-})//end HomeCtrl
-.controller('SettingsCtrl', function($scope,$state){
-
-})//end HomeCtrl
+.controller('ToursCtrl', function($scope) {})//end TourCtrl
+.controller('MapCtrl', function($scope) {})//end MapCtrl
+.controller('SettingsCtrl', function($scope) {})//end SettingsCtrl
