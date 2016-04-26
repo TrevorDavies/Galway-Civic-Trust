d40c90b52635aaaf9d12fd2808f93ea8a8a0bb2c
Ultimatecodelab <adgn353@gmail.com>
2016-04-26 23:26:08 +0100

    cleaned up version

--- Last-Years-App/FYP-master/GalwayCityWaterwayWalks/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Darren\Desktop\backup\GalwayCityWaterwayWalks\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\Darren\Desktop\backup\GalwayCityWaterwayWalks\platforms\android\CordovaLib\AndroidManifest.xml \
--- Last-Years-App/FYP-master/GalwayCityWaterwayWalks/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Darren\Desktop\backup\GalwayCityWaterwayWalks\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Darren\Desktop\backup\GalwayCityWaterwayWalks\platforms\android\AndroidManifest.xml \
